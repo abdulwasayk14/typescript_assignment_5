a12d66b20f91ccd42ce3f0162faf7ceae7bfc98c
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 01:01:20 +0500

    Add files via upload

---
+++ Stripping_name.ts
@@ -0,0 +1,9 @@
+
+
+const personName :string = '    \n\tAbdul wasay Khan\t\n    ';
+
+console.log(personName);
+
+const without_Whitespace :string = personName.trim();
+
+console.log(without_Whitespace);
